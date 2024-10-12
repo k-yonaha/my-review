46a85656222eaef9214165f17fb3c015bc0b01cf
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-10-12 13:34:56 +0900

    fist commit

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <h1>Vue.jsから</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
