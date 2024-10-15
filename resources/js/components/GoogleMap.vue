12d8d2c73f01c67ab79ce70e8a71d779573bf105
kyonakyona0615 <kyonakyona0615@gmail.com>
2024-10-15 11:40:41 +0900

    fix js-api-loader用に修正

--- resources/js/components/GoogleMap.vue
+++ resources/js/components/GoogleMap.vue
@@ -1,23 +1,91 @@
 <script setup>
-import { GoogleMap, AdvancedMarker } from 'vue3-google-map'
+import { shallowRef, toRaw,onMounted } from "vue";
+import { Loader } from "@googlemaps/js-api-loader";
 
-const center = { lat: 40.689247, lng: -74.044502 }
-const markerOptions = { position: center, title: 'LADY LIBERTY' }
-const pinOptions = { background: '#FBBC04' }
-const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY
+let searchText = null;
+const googleMap = shallowRef(null);
+const marker = shallowRef(null);
+
+const loader = new Loader({
+    apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+    version: "weekly",
+    libraries: ["places"],
+    language: "ja",
+});
+
+const mapOptions = {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 4,
+    mapId: "DEMO_MAP_ID",
+};
+onMounted(() => {
+    initMap();
+});
+
+async function initMap() {
+    const { Map } = await loader.importLibrary("maps");
+    const { AdvancedMarkerElement } = await loader.importLibrary("marker");
+
+    googleMap.value = new Map(document.getElementById("map"), mapOptions);
+
+
+    marker.value = new AdvancedMarkerElement({
+        map: googleMap.value,
+        position: { lat: -25.344, lng: 131.031 },
+        title: "Uluru",
+    });
+}
+
+const addressSearch = async () => {
+    const { Geocoder } = await loader.importLibrary("geocoding");
+    const geocoder = new Geocoder();
+
+    geocoder.geocode({ address: searchText }, (results, status) => {
+        marker.setVisible(false);
+        marker.setMap(null);
+        marker = null;
+        if (results) {
+            marker = new vm.google.maps.Marker({
+                map: vm.map,
+                position: results[0].geometry.location,
+                draggable: false,
+            });
+            if (status === "OK") {
+                // map.setPosition({ lat: lat(), lng: lng() });
+                googleMap.setCenter(results[0].geometry.location);
+            }
+        }
+    });
+};
+// const center = { lat: 40.689247, lng: -74.044502 }
+// const markerOptions = { position: center, title: 'LADY LIBERTY' }
+// const pinOptions = { background: '#FBBC04' }
+// const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY
 </script>
 
 <template>
-<!-- TODO
-mapId: 'DEMO_MAP_ID' を使用してもかまいません。DEMO_MAP_ID はテスト専用です。本番環境では DEMO_MAP_ID を使用しないでください。
--->
-  <GoogleMap
-  :api-key="googleApiKey"
-  mapId="DEMO_MAP_ID" 
-  style="width: 100%; height: 500px"
-  :center="center"
-  :zoom="15"
-  >
-    <AdvancedMarker :options="markerOptions" :pin-options="pinOptions"/>
-  </GoogleMap>
+    <div id="map"></div>
+    <div>
+        <input
+            class="px-3 py-2 d-table-cell search-form-address"
+            v-model="searchText"
+            placeholder="Search Address"
+            style="display: block"
+        />
+        <div>
+            <button
+                class="inline-flex p-2 hover:bg-gray-100 rounded"
+                @click="addressSearch"
+            >
+                検索
+            </button>
+        </div>
+    </div>
 </template>
+
+<style>
+#map {
+    height: 100%;
+    width: 100%;
+}
+</style>
